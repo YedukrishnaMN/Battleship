51c9b840a82bad041a7157fe4f3c23c68196cc6d
YedukrishnaMN <yemn22cs@cmrit.ac.in>
2025-06-14 11:04:27 +0530

    Update home.dart

--- home.dart
+++ home.dart
@@ -6,50 +6,67 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(child: Container(
+      body: SafeArea(
+        child: Container(
           decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('b.jpg'),
-                fit: BoxFit.cover,)
-          ),
-      child:Column(
-        children: <Widget>[
-          Row(mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(onPressed: (){
-                    Navigator.pushNamed(context, '/howtoplay');
-                    }, child: Text("Howtoplay",
-              style: TextStyle(
-                fontSize: 15 ,
-                fontWeight: FontWeight.bold,
-                color: Colors.amber
-              ),),
+            image: AssetImage('assets/b.jpg'),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/howtoplay');
+                    },
+                    child: Text(
+                      "Howtoplay",
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      minimumSize: Size(70, 70),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.zero),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 400,
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/game',
+                            arguments: {'sub': true, 's': 10, 't': 10});
+                      },
+                      child: Image.asset(
+                        'assets/starts.png',
+                        width: 100,
+                        height: 100,
+                      ),
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                minimumSize: Size(70, 70),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero
-                ),),),
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.zero,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-          SizedBox(height: 400,),
-          Center(
-            child: Column(mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(onPressed: (){
-                  Navigator.pushReplacementNamed(context,'/game',arguments: {'sub':true,'s':10,'t':10});
-                }, child: Image.asset('starts.png',
-                width: 100,
-                height: 100,),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                        padding: EdgeInsets.zero,),),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
-    ),),);
+    );
   }
 }
-

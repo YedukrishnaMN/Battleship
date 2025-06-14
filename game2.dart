e23c9ddf273ff233356b701416f01adcfba74c3a
YedukrishnaMN <yemn22cs@cmrit.ac.in>
2025-06-14 11:03:59 +0530

    Update game2.dart

--- game2.dart
+++ game2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/pages/classes.dart';
+import 'package:helloworld/classes.dart';
+
 class Game2 extends StatefulWidget {
   const Game2({super.key});
 
@@ -8,220 +9,217 @@ class Game2 extends StatefulWidget {
 }
 
 class _GameState extends State<Game2> {
-
-  void initState(){
+  void initState() {
     super.initState();
+  }
 
+  int selected = 0;
+  Widget img(sh) {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            ships.remove(sh);
+            selected = sh;
+          });
+        },
+        child: Image.asset(
+          'assets/a.jpg',
+          width: 50,
+          height: 50,
+        ));
   }
-  int selected=0;
-  Widget img(sh){
-    return ElevatedButton(onPressed: (){
-      setState(() {
-        ships.remove(sh);
-        selected=sh;
-      });
 
-    }, child: Image.asset('a.jpg',
-      width: 50,
-      height: 50,));
-  }
-  direction(){
-    if(rotate){
-      return 'down.jpg';
-    }
-    else{
-      return 'right.png';
+  direction() {
+    if (rotate) {
+      return 'assets/down.jpg';
+    } else {
+      return 'assets/right.jpg';
     }
   }
-  check(j){
-    if (j.hit==1){
-      return 'blast.png';
-    }
-    else{
-      return image(j.i,j.j);
+
+  check(j) {
+    if (j.hit == 1) {
+      return 'assets/blast.jpg';
+    } else {
+      return image(j.i, j.j);
     }
   }
+
   Widget squares(j) {
-    return ElevatedButton(onPressed: () {},
+    return ElevatedButton(
+        onPressed: () {},
         style: ElevatedButton.styleFrom(
           padding: EdgeInsets.zero,
           minimumSize: Size(30, 30),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.zero, // No rounded corners
           ),
-        ), child: Image.asset(images(j),
+        ),
+        child: Image.asset(
+          images(j),
           width: 30,
-          height: 30,));
+          height: 30,
+        ));
   }
 
   images(j) {
     if (j.hit == 1) {
-      return 'blast.png';
-    }
-    else {
+      return 'assets/blast.jpg';
+    } else {
       if (j.pressed == 1) {
-        return 'd.jpg';
-      }
-      else {
-        return 'water.jpg';
+        return 'assets/d.jpg';
+      } else {
+        return 'assets/water.jpg';
       }
     }
   }
-  Widget square(j){
-    return ElevatedButton(onPressed:(){
-      if(submit){
-        if(selected!=0) {
-          setState(() {
 
-            bool set=true;
-            int row=j.i;
-            int col=j.j;
-            if(rotate) {
-              for (int cn = 0; cn < selected; cn++) {
-                if (b2[row + cn][col]!.mark == 1 || row+cn>=5) {
-                  set = false;
+  Widget square(j) {
+    return ElevatedButton(
+        onPressed: () {
+          if (submit) {
+            if (selected != 0) {
+              setState(() {
+                bool set = true;
+                int row = j.i;
+                int col = j.j;
+                if (rotate) {
+                  for (int cn = 0; cn < selected; cn++) {
+                    if (b2[row + cn][col]!.mark == 1 || row + cn >= 5) {
+                      set = false;
+                    }
+                  }
+                  if (set) {
+                    for (int cn = 0; cn < selected; cn++) {
+                      b2[row + cn][col]!.mark = 1;
+                      b2[row + cn][col]!.pos = cn;
+                    }
+                  }
+                } else if (!rotate) {
+                  for (int cn = 0; cn < selected; cn++) {
+                    if (b2[row][col + cn]!.mark == 1 || col + cn >= 5) {
+                      set = false;
+                    }
+                  }
+
+                  if (set) {
+                    for (int cn = 0; cn < selected; cn++) {
+                      b2[row][col + cn]!.mark = 1;
+                      b2[row][col + cn]!.pos = cn;
+                    }
+                  }
                 }
-              }
-              if (set) {
-                for (int cn = 0; cn < selected; cn++) {
-                  b2[row + cn][col]!.mark = 1;
-                  b2[row + cn][col]!.pos = cn;
+
+                if (set) {
+                  selected = 0;
                 }
-              }
+              });
             }
-            else if(!rotate) {
-              for (int cn = 0; cn < selected; cn++) {
-                if (b2[row][col + cn]!.mark == 1 || col+cn>=5) {
-                  set = false;
-                }
-              }
-
-              if (set) {
-                for (int cn = 0; cn < selected; cn++) {
-                  b2[row][col + cn]!.mark = 1;
-                  b2[row][col + cn]!.pos = cn;
-                }
-              }
-            }
-
-            if(set) {
-              selected = 0;
-            }
-
-          });}
-
-      }
-      else{
-        b![j.i][j.j]!.pressed=1;
-        Navigator.pushReplacementNamed(context, '/game',arguments:{'b1':b,'b2':b2,'sub':false,'s':j.i,'t':j.j,'meh':true,'limit1':limit1,'limit2':limit2});
-      }
-    },
-        style:ElevatedButton.styleFrom(
+          } else {
+            b![j.i][j.j]!.pressed = 1;
+            Navigator.pushReplacementNamed(context, '/game', arguments: {
+              'b1': b,
+              'b2': b2,
+              'sub': false,
+              's': j.i,
+              't': j.j,
+              'meh': true,
+              'limit1': limit1,
+              'limit2': limit2
+            });
+          }
+        },
+        style: ElevatedButton.styleFrom(
           padding: EdgeInsets.zero,
           minimumSize: Size(100, 100),
-          shape:RoundedRectangleBorder(
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.zero, // No rounded corners
           ),
         ),
-        child: Image.asset(check(j),
+        child: Image.asset(
+          check(j),
           width: 100,
           height: 100,
         ));
   }
-  int s=0;
-  int t=0;
-  image(i,j) {
+
+  int s = 0;
+  int t = 0;
+  image(i, j) {
     if (submit) {
       if (b2[i][j]!.ro != null) {
         return b2[i][j]!.ro;
-      }
-      else {
+      } else {
         if (rotate) {
           if (b2[i][j]!.pos == 0) {
-            b2[i][j]!.ro = 'ships30.jpg';
+            b2[i][j]!.ro = 'assets/ships30.jpg';
             return b2[i][j]!.ro;
-          }
-          else if (b2[i][j]!.pos == 1) {
-            b2[i][j]!.ro = 'ships20.jpg';
+          } else if (b2[i][j]!.pos == 1) {
+            b2[i][j]!.ro = 'assets/ships20.jpg';
             return b2[i][j]!.ro;
-          }
-          else if (b2[i][j]!.pos == 2) {
-            b2[i][j]!.ro = 'ships10.jpg';
+          } else if (b2[i][j]!.pos == 2) {
+            b2[i][j]!.ro = 'assets/ships10.jpg';
             return b2[i][j]!.ro;
+          } else {
+            return 'assets/water.jpg';
           }
-          else {
-            return 'water.jpg';
-          }
-        }
-        else {
+        } else {
           if (b2[i][j]!.pos == 0) {
-            b2[i][j]!.ro = 'ships11.jpg';
+            b2[i][j]!.ro = 'assets/ships11.jpg';
             return b2[i][j]!.ro;
-          }
-          else if (b2[i][j]!.pos == 1) {
-            b2[i][j]!.ro = 'ships21.jpg';
+          } else if (b2[i][j]!.pos == 1) {
+            b2[i][j]!.ro = 'assets/ships21.jpg';
             return b2[i][j]!.ro;
-          }
-          else if (b2[i][j]!.pos == 2) {
-            b2[i][j]!.ro = 'ships31.jpg';
+          } else if (b2[i][j]!.pos == 2) {
+            b2[i][j]!.ro = 'assets/ships31.jpg';
             return b2[i][j]!.ro;
-          }
-          else {
-            return 'water.jpg';
+          } else {
+            return 'assets/water.jpg';
           }
         }
       }
-    }
-    else{
-      if(b2[i][j]!.hit==1){
-        return 'blast.png';
-
-      }
-      else{
-        return 'water.jpg';
+    } else {
+      if (b2[i][j]!.hit == 1) {
+        return 'assets/blast.png';
+      } else {
+        return 'assets/water.jpg';
       }
     }
   }
-  List<int> ships=[3,2,2,3];
-  bool rotate=true;
-  List<List<hello?>> b2=show();
-  bool submit=true;
-  Map d={};
+
+  List<int> ships = [3, 2, 2, 3];
+  bool rotate = true;
+  List<List<hello?>> b2 = show();
+  bool submit = true;
+  Map d = {};
   List<List<hello?>>? b;
-  int limit2=10;
-  int limit1=10;
-  int p=1;
-
-
-
+  int limit2 = 10;
+  int limit1 = 10;
+  int p = 1;
 
   @override
   Widget build(BuildContext context) {
-    final arguments=ModalRoute.of(context)!.settings.arguments;
-    d=arguments as Map;
-    submit=d['sub'];
-    b=d['b1'];
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+    d = arguments as Map;
+    submit = d['sub'];
+    b = d['b1'];
 
+    if (!submit) {
+      limit1 = d['limit1'];
+      limit2 = d['limit2'];
+      s = d['s'];
+      t = d['t'];
 
-
-    if(!submit) {
-      limit1=d['limit1'];
-      limit2=d['limit2'];
-      s=d['s'];
-      t=d['t'];
-
-      ships=[];
-      b2=d['b2'];
+      ships = [];
+      b2 = d['b2'];
       if (b2[s][t]!.mark == 1) {
         b2[s][t]!.hit = 1;
-        limit2=limit2-1;
+        limit2 = limit2 - 1;
       }
-      if(limit2==0){
-        Navigator.pushReplacementNamed(context, '/result',arguments: {'p':p});
+      if (limit2 == 0) {
+        Navigator.pushReplacementNamed(context, '/result', arguments: {'p': p});
       }
     }
-
-
 
     return Scaffold(
       body: Column(
@@ -230,32 +228,37 @@ class _GameState extends State<Game2> {
             'PLAYER 2',
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
-          ElevatedButton(onPressed: (){
-            if(submit) {
-              setState(() {
-                rotate = !rotate;
-              });
-            }
-
-          },
-              style:ElevatedButton.styleFrom(
+          ElevatedButton(
+              onPressed: () {
+                if (submit) {
+                  setState(() {
+                    rotate = !rotate;
+                  });
+                }
+              },
+              style: ElevatedButton.styleFrom(
                   padding: EdgeInsets.zero,
                   minimumSize: Size(50, 50),
-                  shape:CircleBorder(
-                  )),
-              child: Image.asset(direction(),
-              width: 30,
-              height: 30,)),
-          Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: ships.map((sh){
+                  shape: CircleBorder()),
+              child: Image.asset(
+                direction(),
+                width: 30,
+                height: 30,
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: ships.map((sh) {
               return img(sh);
-            }).toList(),),
-          Row(mainAxisAlignment: MainAxisAlignment.start,
+            }).toList(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Column(
-                children: b!.map((q){
-                  return Row(mainAxisAlignment: MainAxisAlignment.center,
-                    children: q.map((k){
+                children: b!.map((q) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: q.map((k) {
                       return squares(k);
                     }).toList(),
                   );
@@ -265,9 +268,10 @@ class _GameState extends State<Game2> {
                 width: 360,
               ),
               Column(
-                children: b2.map((q){
-                  return Row(mainAxisAlignment: MainAxisAlignment.center,
-                    children: q.map((k){
+                children: b2.map((q) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: q.map((k) {
                       return square(k);
                     }).toList(),
                   );
@@ -275,14 +279,24 @@ class _GameState extends State<Game2> {
               ),
             ],
           ),
-          ElevatedButton(onPressed: (){
-            if(submit){
-              Navigator.pushNamed(context, '/game',arguments: {'b1':b,'b2':b2,'sub':false,'s':s,'t':t,'meh':false,'limit1':limit1,'limit2':limit2});
-            }
-          }, child: Text("submit"))
+          ElevatedButton(
+              onPressed: () {
+                if (submit) {
+                  Navigator.pushNamed(context, '/game', arguments: {
+                    'b1': b,
+                    'b2': b2,
+                    'sub': false,
+                    's': s,
+                    't': t,
+                    'meh': false,
+                    'limit1': limit1,
+                    'limit2': limit2
+                  });
+                }
+              },
+              child: Text("submit"))
         ],
       ),
-
     );
   }
 }
